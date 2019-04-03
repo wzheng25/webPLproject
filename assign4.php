55b38bf9fe7ebd89eea8345c2d348ff0b6e397f0
wzheng25 <wz2gt@virginia.edu>
2019-04-03 14:32:10 -0400

    Update assign4.php

--- assign4.php
+++ assign4.php
@@ -6,4 +6,20 @@
 // Use control structures (such as selection and loop)
 // Use predefined / standard / built-in function(s)
 
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+	$routeName_input = $_POST['routeName_input']; // validate 
+	$date_input = $_POST['date_input']; // MM/DD/YYYY
+	$distance_input = $_POST['distance_input']; //check if float, always in miles
+	$terrain_input = $_POST['terrain_input']; // don't need validation: drop (road, sidewalk, track, trail, grassfield, turf, sand)
+	$traffic_input = $_POST['traffic_input']; // don't need validation
+	$difficulty_input = $_POST['difficulty_input']; // don't need validation
+	$image_url_input = $_POST['image_url_input']; 
+
+	if (empty($routeName_input)){
+		
+	}
+}
+
 ?>
