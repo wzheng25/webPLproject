0e63431dff3b9157efef0cf97d1ce66c7ca224e6
wz2gt@virginia.edu <wz2gt@virginia.edu>
2019-04-03 23:42:51 -0400

    added a function to external.js

--- js/external.js
+++ js/external.js
@@ -4,4 +4,13 @@
 /* Script used by the onlick of the Comment bubble Img to generate the textbox */
 onButtonClick = () => {
 	document.getElementById('commentbox').className="show";
+	document.getElementById('commentBtn').className="show";
+	document.getElementById('commentImg').className="hide";
+}
+
+var checkComment = function(){
+	if (document.getElementById('commentbox').value==""){
+		alert("Please put something in the comment box");
+	}
+	alert("You entered: " + document.getElementById('commentbox').value);
 }
